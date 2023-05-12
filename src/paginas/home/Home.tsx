35e513c4dcf333ef1904d17db1c96ca2c2eb3283
LarissaMarquesPimenta <laariipimenta@gmail.com>
2023-05-11 21:12:15 -0300

    primeiro commit

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return(
+        <>
+            <h1 className="titulo">Teste</h1>
+            <img src="https://i.pinimg.com/originals/e6/6b/4b/e66b4b7fb4ab868e0be4d3eae496b2bc.jpg" alt="imagem da tela inicial" className="img"/>
+        </>
+    );
+
+}
+
+export default Home; 
