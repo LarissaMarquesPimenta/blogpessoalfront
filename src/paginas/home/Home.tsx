87aac6ff1ca62e628f895e40224453723f0e6979
LarissaMarquesPimenta <laariipimenta@gmail.com>
2023-05-15 15:08:42 -0300

    Grid

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import './Home.css';
-
+import { Grid, Paper } from "@material-ui/core";
 function Home() {
     return(
         <>
-            <h1 className="titulo">Teste</h1>
-            <img src="https://i.pinimg.com/originals/e6/6b/4b/e66b4b7fb4ab868e0be4d3eae496b2bc.jpg" alt="imagem da tela inicial" className="img"/>
-        </>
-    );
+            <Grid container spacing={2}>
+                <Grid item xs = {12} sm = {8} >
+                    <Paper style={{height: "100vh", background: "#C0FDFF"}}/>
+                </Grid>
+                <Grid item container spacing={2} direction="column" xs = {12} sm = {4}> 
+                    <Grid item>
+                        <Paper style={{height: "49vh", background: "#FFCBF2"}}/> 
+                    </Grid>
+                    <Grid item>
+                        <Paper style={{height: "49vh", background: "#E2AFFF"}}/> 
+                    </Grid>
+                </Grid>
 
+            </Grid>
+        </>
+    
+    );
 }
 
 export default Home; 
